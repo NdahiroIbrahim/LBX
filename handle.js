f991ec6c1b2015f4f2013408650dda009c745689
hassan <hassanibrahimndahiro@gmail.com>
2024-12-28 15:16:29 +0300

    initial

---
+++ handle.js
@@ -0,0 +1,9 @@
+
+function handleSubmit() {
+    const button = document.getElementById("submit-button");
+
+    button.addEventListener("click",() => {
+        alert("i was clicked! still in devlopment...")
+    });
+}
+handleSubmit();
