e8c1ca4bcb136e36176ba859aa06e7a7207f4669
hassan <hassanibrahimndahiro@gmail.com>
2025-03-31 17:41:21 +0300

    made one js file to serve all poages

---
+++ waitlist.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // to handle the user waitlist form (trader / investor)
+    const userForm = document.getElementById("waitlistForm");
+    if (userForm) {
+        userForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+            const userData = {
+                name: document.getElementsById("name").value,
+                email:document.getElementById("email").value,
+            };
+
+            fetch("hhtps://lbex-backend.onrender.com/waitlist", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(userData),
+            })
+            .then(response => response.json())
+            .then(data => showPopup(data.message))
+            .catch(error => console.error("Error:", error));
+        });
+    }
+
+    //Handling Business Waitlist Form
+    const businessForm = document.getElementById("businessForm");
+    if (businessForm) {
+        businessForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+            const businessData = {
+                name: document.getElementById("businessName").value,
+                email:document.getElemntById("businessEmail").value,
+                industry: document.getElementById("industry").value,
+                location: document.getElementById("location").value,
+            };
+            fetch("https://lbex-backend.onrender.com/register-business", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(businessData),
+            })
+            .then(response => response.json())
+            .then(data => showPopup(data.message))
+            .catch(error => console.error("Error:", error))
+        });
+    }
+
+    //Function to display the popup message
+
+    function showPopup(message) {
+        const popup = document.createElement("div");
+        popup.classList.add("popup");
+        popup.innerHTML = `
+        <div class="popup-content">
+           <p>${message}</p>
+           <button onclick="closePopup()">OK</button>
+        </div>
+        `;
+        document.body.appendChild(popup);
+    }
+
+    //Function to close the popup
+
+    window.closePopup = function () {
+        const popup = document.querySelector(".popup");
+        if (popup) {
+            popup.remove();
+        }
+    };
+});
